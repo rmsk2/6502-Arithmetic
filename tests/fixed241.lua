da60dda105f4f3689f0acf17467b54662ccd7ca4
Martin Grap <rmsk2@gmx.de>
2023-02-19 14:40:48 +0100

    Add tests for 24 bit fixed point multiplication

---
+++ tests/fixed241.lua
@@ -0,0 +1,5 @@
+require(test_dir .. "arith_base")
+
+valL =   "-0000FF80"
+valR =   "+00014080"
+opRes =  "-013FDFC0"
---
+++ tests/fixed242.lua
@@ -0,0 +1,4 @@
+require(test_dir .. "arith_base_mono")
+
+valL =   "-0002CDC0"
+opRes =  "+07DC5D10"
